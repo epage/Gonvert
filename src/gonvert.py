26a0319cee778b4458d5ff3a29547263b5259abc
Ed Page <eopage@byu.net>
2009-12-07 19:10:28 -0600

    Fixed a bug with fremantle and contexts plus logging device to file

--- src/gonvert.py
+++ src/gonvert.py
@@ -21,6 +21,9 @@ except OSError, e:
 
 logging.basicConfig(level=logging.DEBUG, filename=constants._user_logpath_)
 _moduleLogger.info("gonvert %s-%s" % (constants.__version__, constants.__build__))
+_moduleLogger.info("OS: %s" % (os.uname()[0], ))
+_moduleLogger.info("Kernel: %s (%s) for %s" % os.uname()[2:])
+_moduleLogger.info("Hostname: %s" % os.uname()[1])
 
 
 gonvert_glade.run_gonvert()
