fc07f0d12633b7f6e54dd4f73e5b98db7cb04fbd
Ed Page <eopage@byu.net>
2010-06-05 13:08:26 -0500

    Adding a hint to prefer numbers on the line edit

--- src/gonvert_qt.py
+++ src/gonvert_qt.py
@@ -981,6 +981,7 @@ class UnitWindow(object):
 
 		self._selectedUnitName = QtGui.QLabel()
 		self._selectedUnitValue = QtGui.QLineEdit()
+		self._selectedUnitValue.setInputMethodHints(QtCore.Qt.ImhPreferNumbers)
 		self._selectedUnitValue.textEdited.connect(self._on_value_edited)
 		self._selectedUnitSymbol = QtGui.QLabel()
 
