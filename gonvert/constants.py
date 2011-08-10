b1fec2bd437231ee4a05fc6dc3c79a1d8568ed61
Ed Page <eopage@byu.net>
2011-08-10 07:35:50 -0500

    Bump to 1.1.4-3

--- gonvert/constants.py
+++ gonvert/constants.py
@@ -3,7 +3,7 @@ import os
 __pretty_app_name__ = "Gonvert"
 __app_name__ = "gonvert"
 __version__ = "1.1.4"
-__build__ = 2
+__build__ = 3
 __app_magic__ = 0xdeadbeef
 _data_path_ = os.path.join(os.path.expanduser("~"), ".%s" % __app_name__)
 _user_settings_ = "%s/settings.json" % _data_path_
